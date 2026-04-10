8de1b89e2d8440fee6745dbfa046dbdcf1469a4c
Peng Liu <pdupub@gmail.com>
2026-04-10 11:03:44 +0800

    add new version of minimeta

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,6 +21,20 @@ export default defineConfig({
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/tataufo/peng.pdu.pub' }],
 			sidebar: [
 				{
+					label: 'MiniMeta',
+					// autogenerate: { directory: 'minimeta' },
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: '序言：框架的起点与方法论', slug: 'minimeta/chapter_0' },
+						{ label: '第一章：时间', slug: 'minimeta/chapter_1' },
+						{ label: '第二章：存在与世界', slug: 'minimeta/chapter_2' },
+						{ label: '第三章：生命与智能', slug: 'minimeta/chapter_3' },
+						{ label: '第四章：人与社会', slug: 'minimeta/chapter_4' },
+						{ label: '框架的完整闭环', slug: 'minimeta/chapter_5' },
+						{ label: '框架的哲学地位与边界', slug: 'minimeta/chapter_6' },
+					],
+				},
+				{
 					label: 'WhitePapers',
 					// autogenerate: { directory: 'whitepapers' },
 					items: [
