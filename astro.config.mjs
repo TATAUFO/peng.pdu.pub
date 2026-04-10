d65390e76b381cb0582b56a66a3f65462fd32ee8
Peng Liu <pdupub@gmail.com>
2026-04-10 10:17:24 +0800

    reset dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,8 +32,8 @@ export default defineConfig({
 					],
 				},
 				{
-					label: 'Articles',
-					autogenerate: { directory: 'articles' },
+					label: 'Drafts',
+					autogenerate: { directory: 'drafts' },
 					// items: [
 					// 	// Each item here is one entry in the navigation menu.
 					// 	{ label: 'Example Guide', slug: 'guides/example' },
